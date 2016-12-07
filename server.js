373e1d6e2efee627625b1059f67259bdd37f798b
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2016-12-07 10:07:07 -0800

    Added routes for Contact
    -Added GET route for /contact
    -Added POST route for /contact
    -Installed nodemailer

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '/')));
 
 // Sends the index.html file as response when the '/' route is called
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/contact', function (req, res, next) {
+    res.send('Contact');
+    //res.sendFile(__dirname + '/contact.html');
+});
+
+app.post('/contact', function (req, res, next) {
+    res.send('POST sent to /contact');
 });
 
 app.listen(port, function () {
