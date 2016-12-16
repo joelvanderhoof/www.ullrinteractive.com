b7401b88c7bf30ffb72dd82dc85a655b31ebd92e
Joel Vanderhoof <joelvanderhoof@yahoo.com>
2016-12-16 10:28:27 -0800

    Moved routes to index.js
    -Switched from calling HTTP methods on “app” instance of express to
    “router”
    -Moved routes from server.js to index.js
    -Set up body-parser to run as a route specific callback for url encoded
    requests

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,56 @@
 'use strict';
 
-module.exports = function(app) {
-     var core = require(/*Put controller for contact page here*/);
+var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser'); //Parse request as a JSON object
 
-function sendMail () {
-    var data = {
-        contactEmail: joelvanderhoof@yahoo.com,
-        subject: 'This is the subject',
-        message: 'This is a message'
-    }
-    return data;
-}
-    app.route('/contact').post(/*method from contact page controller that makes the http post with the contact form data*/sendMail);
-}
 
-//Put this in the server controller file
-/*
-var data = req.body;
+var app = express();
+var router = express.Router();
 
-transporter.sendMail({
-    from: data.contactEmail,
-    to: contact@ullerinteractive.com,
-    subject: data.subject,
-    message: data.contactMsg
+
+//Parse application/x-www-form-urlencoded as callback function to be called as middleware for specific route
+var urlencodedParser = bodyParser.urlencoded({ extended: true});
+
+//Namespace api for router
+app.use('/api', router);
+
+// Sends the index.html file as response when the '/' route is called
+router.get('/', function (req, res, next) {
+    res.render('index.pug');
 });
 
-res.json(data);
-*/
+router.get('/contact', function (req, res, next) {
+    res.render('contact.pug');
+});
+
+router.post('/contact', urlencodedParser, function (req, res, next) {
+    //Create transport object for nodemailer to call the sendMail method on
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'joelvanderhoof@gmail.com',
+            pass: 'vivianle'
+        }
+    }),
+        mailOptions = {
+        from: req.body.email,
+        to: 'joelvanderhoof@gmail.com',
+        subject: 'hello',
+        text: req.body.message + '\r\n\r\nName: ' + req.body.name + '\r\nEmail: ' + req.body.email
+    };
+        
+    transporter.sendMail(mailOptions, function (error, response) {
+    //Email not sent
+    if (error) {
+        console.log(error);
+    }
+    //Email sent
+    else {
+        console.log('email sent');
+        res.render('contact.pug');
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,67 +2,22 @@
 
 var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser'); //Parse request as a JSON object
-var cookieParser = require('cookie-parser'); // Parse cookies as a JSON object
 var nodemailer = require('nodemailer');
 
 
 var app = express();
-var router = express.Router();
+var routes = require('./routes/index');
+app.use('/', routes);
+
+//Set view engine
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/public/views');
 
 var port = process.env.PORT || 3000;
 
 //Serves the root directory relative to the directory that the express app is run from
 app.use(express.static(path.join(__dirname, '/public')));
 
-//Parse application/json
-app.use(bodyParser.json());
-//Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-
-//Parse cookies as json data
-app.use(cookieParser());
-
-// Sends the index.html file as response when the '/' route is called
-app.get('/', function (req, res, next) {
-    res.render(__dirname +'/public/views/index.pug');
-});
-
-app.get('/contact', function (req, res, next) {
-    res.render(__dirname +'/public/views/contact.pug');
-});
-
-app.post('/contact', function (req, res, next) {
-    
-    //Create transport object for nodemailer to call the sendMail method on
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'joelvanderhoof@gmail.com',
-            pass: 'vivianle'
-        }
-    });
-    
-    //
-    var mailOptions = {
-        from: req.body.email,
-        to: 'joelvanderhoof@gmail.com',
-        subject: 'hello',
-        text: req.body.message + '\r\n\r\nName: ' + req.body.name + '\r\nEmail: ' + req.body.email
-    };
-        
-    transporter.sendMail(mailOptions, function (error, response) {
-    //Email not sent
-    if (error) {
-        console.log(error);
-    }
-    //Email sent
-    else {
-        console.log('email sent');
-        res.render(__dirname +'/public/views/contact.pug');
-    }
-  });
-});
 
 app.listen(port, function () {
     console.log('The server is listening on port ' + port);
